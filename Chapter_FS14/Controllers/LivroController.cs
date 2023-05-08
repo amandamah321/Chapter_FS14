9cb757d2c8f9c1c7de20182903d2bdb24e6d6193
amandamah321 <amanda_mah321@hotmail.com>
2023-05-08 09:36:14 +0200

    Adicionar arquivos de projeto.

---
+++ Chapter_FS14/Controllers/LivroController.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using Chapter_FS14.Models;
+using Chapter_FS14.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chapter_FS14.Controllers
+{
+
+    [Produces("application/json")]
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LivroController : ControllerBase
+    {
+        private readonly LivroRepository _livroRepository;
+
+        public LivroController(LivroRepository livroRepository)
+        { 
+            _livroRepository = livroRepository;
+        }
+
+        [HttpGet]
+
+        public IActionResult listar()
+        {
+            try  
+            {
+                return Ok(_livroRepository.Listar());  
+            }
+            catch (Exception e) 
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+    }
+}
---
+++ Chapter_FS14/Repository/LivroRepository.cs
@@ -0,0 +1,22 @@
+﻿using Chapter_FS14.Contexts;
+using Chapter_FS14.Models;
+using Microsoft.Identity.Client;
+
+namespace Chapter_FS14.Repository
+{
+    public class LivroRepository
+    {
+
+        private readonly Sqlcontext _context;
+        public LivroRepository(Sqlcontext context)
+        {
+            _context = context;
+        }
+
+        public List<Livro> Listar()
+        {
+            return _context.Livros.ToList();
+        }
+
+    }
+}
